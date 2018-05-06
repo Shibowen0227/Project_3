05b83b355a77ee0c89bdf561b7f092c514e428e1
Shibowen0227 <javagithub@163.com>
2018-05-06 10:37:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shi/project_3/AnotherActivity.java
@@ -0,0 +1,11 @@
+package com.example.shi.project_3;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
